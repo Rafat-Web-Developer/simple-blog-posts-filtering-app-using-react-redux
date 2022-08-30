163783d8822fd09690a0129178c06dda3936bb2a
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-08-30 09:49:35 +0600

    Add more data

--- src/redux/blog/initialState.js
+++ src/redux/blog/initialState.js
@@ -44,6 +44,24 @@ const initialState = [
     author_img: "https://avatars.githubusercontent.com/u/73503432?v=4",
     publication_date: "11 Jul, 2022",
   },
+  {
+    id: 6,
+    title: "Debounce trick using react",
+    category: "Education",
+    img: "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    author: "Rafat Hossain",
+    author_img: "https://avatars.githubusercontent.com/u/73503432?v=4",
+    publication_date: "11 Jul, 2022",
+  },
+  {
+    id: 7,
+    title: "Learn Redux With React",
+    category: "Education",
+    img: "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    author: "Jubair Ahmed Junjun",
+    author_img: "https://avatars.githubusercontent.com/u/73503432?v=4",
+    publication_date: "11 Jul, 2022",
+  },
 ];
 
 export default initialState;
